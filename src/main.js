c9de011867e722cb5c6c4bb7f67a983e0ef182de
vineelgajjala <gajjala.vineel@gmail.com>
2021-07-20 01:42:34 -0500

    set up vue2 sidebar

--- src/main.js
+++ src/main.js
@@ -4,11 +4,14 @@ import vuetify from './plugins/vuetify'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import Vue2Sidebar from 'vue2-sidebar';
+
 
 
 library.add(faUserSecret)
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
+Vue.use(Vue2Sidebar);
 
 
 
