8519c55dc83c4914c0932cdeefc351b396df1d7c
vineelgajjala <gajjala.vineel@gmail.com>
2021-07-23 22:33:06 -0500

    added footer

--- src/App.vue
+++ src/App.vue
@@ -5,17 +5,23 @@
     </v-app-bar>
 
    <router-view></router-view>
+  
+   <v-footer>
+     <Footer/>
+   </v-footer>
   </v-app>
 </template>
 
 <script>
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 export default {
   name: 'App',
 
   components: {
-    Header
+    Header,
+    Footer
   },
 
   data: () => ({
