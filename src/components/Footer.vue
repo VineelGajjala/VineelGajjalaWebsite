dbbd9b6423c425d08714060b8df41cb0dde1775c
vineelgajjala <gajjala.vineel@gmail.com>
2021-07-24 00:28:06 -0500

    working on adding hyperlinks to icons

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,19 +1,35 @@
 <template>
   <v-container>
-    <v-footer padless>
-      <v-card flat class="text-center">
+    <v-footer
+      padless
+    >
+      <v-card
+        flat
+        tile
+        class="white--text text-center"
+      >
         <v-card-text>
-          <v-btn v-for="icon in icons" :key="icon" class="mx-4" icon>
-            <v-icon size="24px">{{ icon }}</v-icon>
+          <v-btn
+            v-for="(icon) in icons"
+            :key="icon"
+            class="mx-4 black--text"
+            icon
+          >
+
+            <v-icon size="24px">
+              {{ icon }}
+            </v-icon>
           </v-btn>
         </v-card-text>
-        <v-card-text
-          class="pt-0"
-        >Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet. Mauris cursus commodo interdum. Praesent ut risus eget metus luctus accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim a sit amet dui. Duis commodo vitae velit et faucibus. Morbi vehicula lacinia malesuada. Nulla placerat augue vel ipsum ultrices, cursus iaculis dui sollicitudin. Vestibulum eu ipsum vel diam elementum tempor vel ut orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</v-card-text>
+
+        <v-card-text class="black--text pt-0">
+          Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet. Mauris cursus commodo interdum. Praesent ut risus eget metus luctus accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim a sit amet dui. Duis commodo vitae velit et faucibus. Morbi vehicula lacinia malesuada. Nulla placerat augue vel ipsum ultrices, cursus iaculis dui sollicitudin. Vestibulum eu ipsum vel diam elementum tempor vel ut orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+        </v-card-text>
+
         <v-divider></v-divider>
-        <v-card-text>
-          {{ new Date().getFullYear() }} —
-          <strong>Photography Vuetify Project</strong>
+
+        <v-card-text class="black--text">
+          {{ new Date().getFullYear() }} — <strong>Vineel Reddy Hari Gajjala</strong>
         </v-card-text>
       </v-card>
     </v-footer>
@@ -21,15 +37,21 @@
 </template>
 
 <script>
-export default {
-  name: "Header",
-  data: () => ({
-    icons: [
-      "fab fa-facebook",
-      "fab fa-google-plus",
-      "fab fa-linkedin",
-      "fab fa-instagram"
-    ]
-  })
-};
+  export default {
+    name: 'Footer',
+    data: () => ({
+      icons: [
+        'mdi-facebook',
+        'mdi-github',
+        'mdi-linkedin',
+        'mdi-instagram',
+      ],
+      hyperlinks: [
+        "https://www.facebook.com/vineelreddy.gajjala",
+        "https://github.com/VineelGajjala",
+        "https://www.linkedin.com/in/vineel-gajjala/",
+        "https://www.instagram.com/vineelgajjala/?hl=en",
+      ],
+    }),
+  }
 </script>
