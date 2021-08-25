b4ab800d708cfe80564d73e0143b4be3632ff68c
vineelgajjala <gajjala.vineel@gmail.com>
2021-08-24 23:47:10 -0500

    finished not found and updated footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -23,7 +23,7 @@
         </v-card-text>
 
         <v-card-text class="black--text pt-0">
-          Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet. Mauris cursus commodo interdum. Praesent ut risus eget metus luctus accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim a sit amet dui. Duis commodo vitae velit et faucibus. Morbi vehicula lacinia malesuada. Nulla placerat augue vel ipsum ultrices, cursus iaculis dui sollicitudin. Vestibulum eu ipsum vel diam elementum tempor vel ut orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+          Images are courtesy of <a href="https://www.freepik.com/"> freepik.com</a>. Vineel Reddy Gajjala is a current College Sophmore at the University of Texas at Austin in the Computer Science department! You can contact him via his email at gajjala.vineel@gmail.com or via any of the social medias listed above!
         </v-card-text>
 
         <v-divider></v-divider>
--- src/components/views/NotFound.vue
+++ src/components/views/NotFound.vue
@@ -1,6 +1,8 @@
-<template lang='pug'>
-  div
-    h1 Not found
+<template>
+ <div class="notFound">
+   <h1> Page Not Found </h1>
+   <h5> Try Clicking a button on the toolbar to get to a page!</h5>
+  </div>
 </template>
 
 <script>
@@ -8,3 +10,18 @@
     name: 'NotFound',
   }
 </script>
+
+<style scoped>
+h1 {
+  font-family: arial;
+  font-weight: bold;
+  text-align: center;
+  font-size: 100px;
+}
+
+h5 {
+  font-family: arial;
+  text-align: center;
+}
+
+</style>
